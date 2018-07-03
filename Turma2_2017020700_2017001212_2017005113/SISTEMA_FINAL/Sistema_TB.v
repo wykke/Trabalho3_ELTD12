a8b13d8b9e37ffc569ae156356b60dafd900f423
WyKKe <alexjp17@gmail.com>
2018-07-03 18:32:09 -0300

    [ADD] - Atualizado arquivo do testbench

--- Turma2_2017020700_2017001212_2017005113/SISTEMA_FINAL/Sistema_TB.v
+++ Turma2_2017020700_2017001212_2017005113/SISTEMA_FINAL/Sistema_TB.v
@@ -2,11 +2,12 @@
 module Sistema_TB ;
 	
 	reg clk;
-	wire [15:0] saida;
+	wire [15:0] saida, saidaResto;
 	
 	SISTEMA_FINAL S0 (
 	.clk 		(clk),
-	.saida	(saida)
+	.saida	(saida),
+	.saidaResto (saidaResto)
 	);
 	
 	//integer repeticoes;
@@ -23,8 +24,8 @@ module Sistema_TB ;
 	end
 	
 	initial begin
-		$display("\t\ttime,\tclk,\tSaida");
-		$monitor("%d, \t%b, \t%d", $time, clk, saida); 
+		$display("\t\ttime,\tclk,\tSaida,\tResto");
+		$monitor("%d, \t%b, \t%d, \t%d", $time, clk, saida, saidaResto); 
 	end
 	
 	initial
